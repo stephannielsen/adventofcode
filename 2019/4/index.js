3d36f9da1a93a4713f95331740fe3377bbf2963c
Stephan Nielsen <stephannielsen@gmail.com>
2019-12-04 21:47:15 +0100

    Day 4, Part 2.

--- 2019/4/index.js
+++ 2019/4/index.js
@@ -10,6 +10,22 @@ const adjacentDigits = n => {
         return (b === arr[index-1])
     })
 }
+const min2adjacents = n => {
+    return n.toString().split('').reduce((a, b, index, arr) => {
+        if (index === 0) { 
+            a.push(1)
+            return a
+        }
+        if (arr[index-1] === b) {
+            a[a.length-1] = a[a.length-1] + 1
+            return a
+        }
+        else {
+            a.push(1)
+            return a
+        }
+    }, []).some(x => x === 2)
+}
 const neverDecrease = n => {
     return n.toString().split('').reduce((a, b, index, arr) => {
         if (index === 0) return true
@@ -19,18 +35,22 @@ const neverDecrease = n => {
 }
 
 const sampleTrue = 223456
-const sampleFalse = 171310
+const sampleFalse = 111222
 
 console.log(sixDigits(sampleTrue))
 console.log(inRange(sampleTrue, start, end))
 console.log(adjacentDigits(sampleTrue))
 console.log(neverDecrease(sampleTrue))
+console.log(min2adjacents(sampleTrue))
 
 console.log(sixDigits(sampleFalse))
 console.log(inRange(sampleFalse, start, end))
 console.log(adjacentDigits(sampleFalse))
 console.log(neverDecrease(sampleFalse))
+console.log(min2adjacents(sampleFalse))
 
+
+// Part 1
 let count = 0
 for (let i = start; i <= end; i++) {
     if (sixDigits(i) && inRange(i, start, end) && adjacentDigits(i) && neverDecrease(i)) {
@@ -38,5 +58,14 @@ for (let i = start; i <= end; i++) {
     }
 }
 
-// Part 1
+console.log(count)
+
+// Part 2
+count = 0
+for (let i = start; i <= end; i++) {
+    if (sixDigits(i) && inRange(i, start, end) && adjacentDigits(i) && neverDecrease(i) && min2adjacents(i)) {
+        count++
+    }
+}
+
 console.log(count)
